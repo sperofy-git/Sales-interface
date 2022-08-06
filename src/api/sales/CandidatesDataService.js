825a7a271b2e647c603ba96ab4246bfabfc3e4fd
kvsk <contact@sperofy.com>
2022-08-06 18:09:31 +0530

    added delete service functionality

--- src/api/sales/CandidatesDataService.js
+++ src/api/sales/CandidatesDataService.js
@@ -5,6 +5,10 @@ class CandidatesDataService {
     executeAllCandidatesDS(name) {
         return axios.get(`http://localhost:8080/users/${name}/all_listCandidates`)
      }
+
+     removeCandidate(name,id) {
+        return axios.delete(`http://localhost:8080/users/${name}/candidates/${id}`)
+     }
  
 
 }
--- src/components/sales/candidates/ListCandidatesComponent.jsx
+++ src/components/sales/candidates/ListCandidatesComponent.jsx
@@ -14,25 +14,61 @@ class ListCandidatesComponent extends Component {
             //  { ID: 1003 , Name: "Prakash Miriyala", PrimarySkill: "Linux", SecondarySkill: "Middleware"},
             //  { ID: 1004 , Name: "Srini Kodi", PrimarySkill: ".Net", SecondarySkill: "PHP"},
             //  { ID: 1005 , Name: "Pedro", PrimarySkill: "React", SecondarySkill: "Angular"}
-            ]
+            ],
+            message: null
+
         }
+
+        this.deleteClicked = this.deleteClicked.bind(this)
+        this.updateClicked = this.updateClicked.bind(this)
+        this.refreshCandidateList = this.refreshCandidateList.bind(this)
+
     }
 
     componentDidMount () {
+        this.refreshCandidateList()
+    }
+
+    refreshCandidateList () {
         let username = Authentication.getLoggedUser()
 
         CandidatesDataService.executeAllCandidatesDS(username).then(
             response => {
-                console.log(response.data)
+               // console.log(response.data)
             this.setState( {
                 candidates : response.data
             }) }
         )
     }
+
+    deleteClicked(id) {
+        let username = Authentication.getLoggedUser()
+
+       // console.log('Delete Button Clicked ' + id +" "+username) 
+       CandidatesDataService.removeCandidate(username,id).then(
+           reponse => {
+               this.setState({
+                   message : `Candidate ${id} successfully deleted`
+                   
+               })
+               this.refreshCandidateList()
+           }
+       )
+
+    }
+
+    updateClicked(id) {
+        let username = Authentication.getLoggedUser()
+
+       // console.log('Update Button Clicked ' + id + " " + username)
+
+    }
+
     render() {
         return (
             <div className="container">
                 <h1> Current Available Candidates </h1>
+                {this.state.message && <div className='alert alert-success'>{this.state.message}</div>}
                 <table className="table">
                     <thead>
                         <tr>
@@ -53,8 +89,8 @@ class ListCandidatesComponent extends Component {
                                 <td>{candidate.name} </td>
                                 <td>{candidate.primarySkill} </td>
                                 <td>{candidate.secondarySkill} </td>
-                                <td><button>Delete</button></td>
-                                <td><button>Update</button></td>
+                                <td><button className='btn btn-warning' onClick={() => this.deleteClicked(candidate.id)}>Delete</button></td>
+                                <td><button className='btn btn-success' onClick={() => this.updateClicked(candidate.id)}>Update</button></td>
                                 </tr>
                              )
                         }                   
